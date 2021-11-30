af1de78f710a7fbebc8a3640dca3e932706487cd
karthikvarada-repo <95250916+karthikvarada-repo@users.noreply.github.com>
2021-11-30 11:57:29 -0600

    Update test_volume_cuboid.py

--- test_volume_cuboid.py
+++ test_volume_cuboid.py
@@ -4,5 +4,5 @@ class TestCuboid(unittest.TestCase):
     def test_volume(self):
         self.assertAlmostEqual(cuboid_volume(2),8)
         self.assertAlmostEqual(cuboid_volume(1),1)
-        self.assertAlmostEqual(cuboid_volume(0),0)
+        self.assertAlmostEqual(cuboid_volume(0),25)
         self.assertAlmostEqual(cuboid_volume(5.5),166.375)
